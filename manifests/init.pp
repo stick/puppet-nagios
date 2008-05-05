422f4ed7a943f96905d2f0b8ef7f034f3e73d00d
Chris MacLeod <cmacleod@redhat.com>
2008-05-05 11:45:45 -0400

    wrong side for ?

--- manifests/init.pp
+++ manifests/init.pp
@@ -64,7 +64,7 @@ class nagios::client inherits nagios {
     $swap_critical = $swap_critical ? { '' => '2%', default => $swap_critical }
 
     # case this if you need it
-    $nagios_muninhost = ? $nagios_muninhost { "" => "munin.util.${site}", default => $nagios_munin_host }
+    $nagios_muninhost = $nagios_muninhost ? { "" => "munin.util.${site}", default => $nagios_munin_host }
 
     package { "nrpe":
         name    => $operatingsystemrelease ? {
